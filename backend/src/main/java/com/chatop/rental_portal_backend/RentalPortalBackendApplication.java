406da43672ca00aaa3e5f594cf6169b64ebbc4be
André Bacaliu <andre.bacaliu@icloud.com>
2024-12-10 14:46:36 +0100

    chore:Initialize Spring Boot project with dependencies

---
+++ backend/src/main/java/com/chatop/rental_portal_backend/RentalPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chatop.rental_portal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalPortalBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/chatop/rental_portal_backend/RentalPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatop.rental_portal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
