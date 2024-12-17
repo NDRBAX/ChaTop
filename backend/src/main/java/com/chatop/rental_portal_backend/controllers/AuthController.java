6ac9556f95d075d2a6b3420a38c64d0eeda0a0e8
André Bacaliu <andre.bacaliu@icloud.com>
2024-12-17 17:07:36 +0100

    feat: Add CrossOrigin annotation to AuthController for CORS support
    - Added @CrossOrigin annotation to the AuthController to allow requests
    from 'http://localhost:4200'

--- backend/src/main/java/com/chatop/rental_portal_backend/controllers/AuthController.java
+++ backend/src/main/java/com/chatop/rental_portal_backend/controllers/AuthController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     public final JWTService jwtService;
     private final AuthService authService;
