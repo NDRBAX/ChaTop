2f1674b1b62de0bebbd7429016de59063976cd11
André Bacaliu <andre.bacaliu@icloud.com>
2024-12-17 15:11:54 +0100

    feat: Add User entity for user management
    - Created a new User entity class for managing user data in the database
    - Included fields for id, name, email, password, createdAt, and
    updatedAt
    - Added validation constraints for name, email, and password using
    annotations
    - Implemented timestamp management for createdAt and updatedAt with
    @PrePersist and @PreUpdate
    - Configured the entity to be stored in the "USERS" table with unique
    email constraint

---
+++ backend/src/main/java/com/chatop/rental_portal_backend/models/User.java
@@ -0,0 +1,52 @@
+package com.chatop.rental_portal_backend.models;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    // Met à jour la date de modification
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
+    }
+}
