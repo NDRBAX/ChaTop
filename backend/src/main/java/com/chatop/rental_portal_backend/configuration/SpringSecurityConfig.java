4d0f552d8fe5e6baf0b72ceb6879594e4c05472a
André Bacaliu <andre.bacaliu@icloud.com>
2024-12-17 15:06:16 +0100

    refactor: Enhance Spring Security configuration with JWT and custom user details service
    - Replaced hardcoded JWT secret key with a property-based approach using
    @Value
    - Added custom user details service for user authentication management
    - Simplified and updated the security filter chain to permit
    registration and login endpoints
    - Removed unused in-memory user details manager
    - Added AuthenticationManager bean to integrate custom user details
    service with authentication
    - Improved JWT decoder setup with a more flexible secret key
    configuration

--- backend/src/main/java/com/chatop/rental_portal_backend/configuration/SpringSecurityConfig.java
+++ backend/src/main/java/com/chatop/rental_portal_backend/configuration/SpringSecurityConfig.java
@@ -2,43 +2,34 @@ package com.chatop.rental_portal_backend.configuration;
 
 import javax.crypto.spec.SecretKeySpec;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.chatop.rental_portal_backend.services.CustomUserDetailsService;
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
 @Configuration
 public class SpringSecurityConfig {
-    private String jwtKey = "mlsdfskfLKJLkEEç383093lK3ODI039JD3IPLMK3PD3DKJ3ND3I8JDOK?lk";
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
-                .build();
-    }
+    @Value("${jwt.secret}")
+    private String jwtKey;
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        SecretKeySpec secretKey = new SecretKeySpec(this.jwtKey.getBytes(), 0, this.jwtKey.getBytes().length, "RSA");
+        SecretKeySpec secretKey = new SecretKeySpec(this.jwtKey.getBytes(), 0,
+                this.jwtKey.getBytes().length, "RSA");
         return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
     }
 
@@ -52,11 +43,30 @@ public class SpringSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // Mettre utilisateurs en mémoire
     @Bean
-    public UserDetailsService users() {
-        UserDetails user = User.builder().username("user").password(passwordEncoder().encode("password")).roles("USER")
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/auth/register", "/auth/login").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
-        return new InMemoryUserDetailsManager(user);
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, CustomUserDetailsService userDetailsService)
+            throws Exception {
+
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+
+        return authenticationManagerBuilder.build();
+    }
+
 }
