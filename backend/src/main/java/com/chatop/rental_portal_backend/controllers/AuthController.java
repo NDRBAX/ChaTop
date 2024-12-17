8c18e35b76026681aa8a2f4532928c3c0e1cc315
André Bacaliu <andre.bacaliu@icloud.com>
2024-12-17 15:19:14 +0100

    feat: Add AuthController for authentication and user management
    - Introduced a new AuthController class in the controllers package.
    - Added three endpoints:
      - POST /auth/login: Handles user login and returns a JWT token.
    - POST /auth/register: Handles user registration and returns a JWT
    token.
      - GET /auth/me: Retrieves details of the authenticated user.
    - Integrated JWTService and AuthService for authentication logic.
    - Included DTOs for UserLogin, UserRegister, and AuthenticatedUser for
    structured data handling.
    - Applied validation to request bodies using the @Valid annotation.

---
+++ backend/src/main/java/com/chatop/rental_portal_backend/controllers/AuthController.java
@@ -0,0 +1,69 @@
+package com.chatop.rental_portal_backend.controllers;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chatop.rental_portal_backend.services.JWTService;
+import com.chatop.rental_portal_backend.services.AuthService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.chatop.rental_portal_backend.dto.AuthenticatedUserDTO;
+import com.chatop.rental_portal_backend.dto.UserLoginDTO;
+import com.chatop.rental_portal_backend.dto.UserRegisterDTO;
+
+@RestController
+public class AuthController {
+    public final JWTService jwtService;
+    private final AuthService authService;
+
+    public AuthController(JWTService jwtService, AuthService authService) {
+        this.jwtService = jwtService;
+        this.authService = authService;
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<Map<String, String>> login(@Valid @RequestBody UserLoginDTO userLoginDTO) {
+        try {
+            String token = authService.loginUser(userLoginDTO);
+
+            Map<String, String> response = Map.of("token", token);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            // Sinon -> erreur 401
+            return ResponseEntity.status(401).body(Map.of("message", e.getMessage()));
+        }
+    }
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<Map<String, String>> register(@Valid @RequestBody UserRegisterDTO userRegisterDTO) {
+        try {
+            // Enregistrement de l'utilisateur
+            String token = authService.registerUser(userRegisterDTO);
+
+            // Répondre avec un token JWT
+            Map<String, String> response = Map.of("token", token);
+            return ResponseEntity.ok(response);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).body(Map.of());
+        }
+    }
+
+    @GetMapping("/auth/me")
+    public ResponseEntity<AuthenticatedUserDTO> getAuthenticatedUser() {
+        try {
+            AuthenticatedUserDTO authenticatedUserDTO = authService.getAuthenticatedUser();
+            return new ResponseEntity<>(authenticatedUserDTO, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
