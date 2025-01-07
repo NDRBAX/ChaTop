5a42755d874f0c224cc320d3b047dad827290821
André Bacaliu <andre.bacaliu@icloud.com>
2025-01-07 17:09:48 +0100

    feat: Add SendMessageMapper interface for mapping SendMessageDTO to Message
    - Created the SendMessageMapper interface to map SendMessageDTO to
    Message entity
    - Implemented helper methods for mapping rental and user IDs to their
    respective entities using RentalRepository and UserRepository
    - Used componentModel = "spring" to allow Spring dependency injection in
    the mapping process

---
+++ backend/src/main/java/com/chatop/rental_portal_backend/mappers/SendMessageMapper.java
@@ -0,0 +1,43 @@
+package com.chatop.rental_portal_backend.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.chatop.rental_portal_backend.dto.SendMessageDTO;
+import com.chatop.rental_portal_backend.models.Message;
+import com.chatop.rental_portal_backend.models.Rental;
+import com.chatop.rental_portal_backend.models.User;
+import com.chatop.rental_portal_backend.repositories.RentalRepository;
+import com.chatop.rental_portal_backend.repositories.UserRepository;
+
+@Mapper(componentModel = "spring", uses = { SendMessageMapper.Helper.class })
+public interface SendMessageMapper {
+    SendMessageMapper INSTANCE = Mappers.getMapper(SendMessageMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "created_at", ignore = true)
+    @Mapping(target = "updated_at", ignore = true)
+    @Mapping(target = "rental", source = "rental_id")
+    @Mapping(target = "user", source = "user_id")
+    Message sendMessageDTOToMessage(SendMessageDTO sendMessageDTO);
+
+    @Component
+    class Helper {
+        @Autowired
+        private RentalRepository rentalRepository;
+
+        @Autowired
+        private UserRepository userRepository;
+
+        public Rental mapRental(int rental_id) {
+            return rentalRepository.findById(rental_id).orElse(null);
+        }
+
+        public User mapUser(int user_id) {
+            return userRepository.findById(user_id).orElse(null);
+        }
+    }
+}
