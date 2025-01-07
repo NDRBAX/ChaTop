92b05cdb79189a1839cc0708327aa01377c7acf9
André Bacaliu <andre.bacaliu@icloud.com>
2025-01-07 11:50:34 +0100

    refactor: Remove unnecessary Lombok annotations from AuthenticatedUserDTO
    - Removed @NoArgsConstructor and @AllArgsConstructor annotations from
    AuthenticatedUserDTO
    - Kept @Data annotation to maintain required getters, setters, and other
    Lombok features

--- backend/src/main/java/com/chatop/rental_portal_backend/dto/AuthenticatedUserDTO.java
+++ backend/src/main/java/com/chatop/rental_portal_backend/dto/AuthenticatedUserDTO.java
@@ -2,13 +2,9 @@ package com.chatop.rental_portal_backend.dto;
 
 import java.sql.Timestamp;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class AuthenticatedUserDTO {
     private Integer id;
     private String name;
