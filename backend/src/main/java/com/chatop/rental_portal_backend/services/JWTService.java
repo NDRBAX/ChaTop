7dc29ffc41b2618c64c569f3bb9c6214ae68d1ea
André Bacaliu <andre.bacaliu@icloud.com>
2025-01-07 11:36:38 +0100

    feat: Enhance JWT service with token validation and decoding methods
    - Renamed class from 'JWTService' to 'JwtService'
    - Added 'logger' for logging JWT generation and validation processes
    - Introduced 'validateToken' method to validate JWT tokens
    - Introduced 'decodeToken' method to decode JWT tokens with error
    handling
    - Updated constructor to inject 'JwtDecoder' in addition to 'JwtEncoder'

--- backend/src/main/java/com/chatop/rental_portal_backend/services/JWTService.java
+++ backend/src/main/java/com/chatop/rental_portal_backend/services/JWTService.java
@@ -3,35 +3,43 @@ package com.chatop.rental_portal_backend.services;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.security.oauth2.jwt.JwtValidationException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JWTService {
+public class JwtService {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtService.class);
+
+    private final JwtDecoder jwtDecoder;
     private final JwtEncoder jwtEncoder;
 
-    public JWTService(JwtEncoder jwtEncoder) {
+    public JwtService(JwtDecoder jwtDecoder, JwtEncoder jwtEncoder) {
+        this.jwtDecoder = jwtDecoder;
         this.jwtEncoder = jwtEncoder;
     }
 
     public String generateToken(Authentication authentication) {
-        // Calcule la date d'expiration du token en ajoutant 1 heure à l'instant actuel
+        logger.info("### Generating JWT token for user: {} ###", authentication.getName());
+
         Instant now = Instant.now();
         Instant expiration = now.plus(1, ChronoUnit.HOURS);
 
         JwtClaimsSet claims = JwtClaimsSet.builder()
-                // Définit le sujet du JWT, souvent l'identifiant unique de l'utilisateur
                 .subject(authentication.getName())
-                // Ajoute une revendication pour indiquer la date d'émission du token
                 .issuedAt(now)
-                // Ajoute une revendication pour indiquer la date d'expiration du token
                 .expiresAt(expiration)
-                // Construit l'objet JwtClaimsSet qui contient les informations du JWT
                 .build();
 
         JwsHeader header = JwsHeader.with(MacAlgorithm.HS256)
@@ -43,4 +51,28 @@ public class JWTService {
 
         return jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
     }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtDecoder.decode(token);
+            return true;
+        } catch (JwtValidationException e) {
+            logger.error("### Invalid JWT token: {} ###", token);
+            return false;
+        } catch (JwtException e) {
+            logger.error("### Error during token validation: {} ###", e.getMessage());
+            throw new RuntimeException("Unexpected error during token validation", e);
+        }
+    }
+
+    public Jwt decodeToken(String token) {
+        try {
+            logger.info("### Decoding JWT token: {} ###", token);
+            return jwtDecoder.decode(token);
+        } catch (JwtException e) {
+            logger.error("### Invalid JWT token: {} ###", token);
+            throw new RuntimeException("Invalid token: " + e.getMessage(), e);
+        }
+    }
+
 }
