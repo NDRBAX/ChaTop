7e5f8fa60232d1f93777c452dd675fd6b05ea280
André Bacaliu <andre.bacaliu@icloud.com>
2025-01-14 17:22:00 +0100

    feat: Add Swagger documentation to message endpoint
    - Added @Tag annotation to the MessageController for "Messages"
    endpoints
    - Introduced @Operation, @ApiResponse, and related Swagger annotations
    to the sendMessage method for detailed API documentation
    - Defined responses for successful and unauthorized message sending with
    corresponding DTOs (ResponseMessageDTO and EmptyResponseDTO)

--- backend/src/main/java/com/chatop/rental_portal_backend/controllers/MessageController.java
+++ backend/src/main/java/com/chatop/rental_portal_backend/controllers/MessageController.java
@@ -6,16 +6,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chatop.rental_portal_backend.dto.EmptyResponseDTO;
 import com.chatop.rental_portal_backend.dto.MessageRequestDTO;
 import com.chatop.rental_portal_backend.dto.ResponseMessageDTO;
 import com.chatop.rental_portal_backend.services.IMessageService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/messages")
+@Tag(name = "Messages", description = "Endpoints for message management")
 public class MessageController {
 
     private final IMessageService messageService;
@@ -24,6 +31,24 @@ public class MessageController {
         this.messageService = messageService;
     }
 
+    @Operation(
+        summary = "Send a message",
+        description = "Sends a message to a user.",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Message sent successfully", 
+                content = @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ResponseMessageDTO.class))
+            ),
+            @ApiResponse(
+                responseCode = "401",
+                description = "Unauthorized",
+                content = @Content(mediaType = "application/json",
+                schema = @Schema(implementation = EmptyResponseDTO.class))
+            )
+        }
+    )
     @PostMapping
     public ResponseEntity<ResponseMessageDTO> sendMessage(@Valid @RequestBody MessageRequestDTO messageRequest) {
         log.info("Received message: {}", messageRequest);
