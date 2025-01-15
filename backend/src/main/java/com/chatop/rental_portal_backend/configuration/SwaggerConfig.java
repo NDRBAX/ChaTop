c59834740499aa74635ddf6dc750bd4a15eebbfc
André Bacaliu <andre.bacaliu@icloud.com>
2025-01-15 22:15:22 +0100

    feat: Enhance Swagger configuration with JWT security scheme
    - Added a JWT security scheme to the Swagger configuration using OpenAPI
    components
    - Updated the OpenAPI bean to include the security scheme 'bearer-key'
    with HTTP type and bearer format
    - Removed the unused license information from the OpenAPI info section
    - Maintained logging for Swagger configuration initialization

--- backend/src/main/java/com/chatop/rental_portal_backend/configuration/SwaggerConfig.java
+++ backend/src/main/java/com/chatop/rental_portal_backend/configuration/SwaggerConfig.java
@@ -4,25 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Profile("dev")
 @Configuration
 public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI rentalPortalOpenAPI() {
-        log.info("### SWAGGER CONFIGURATION ###");
-        return new OpenAPI()
-                .info(new Info().title("Rental Portal API")
-                        .description(
-                                "Documentation for Rental Portal API to mangage rentals properties and users in Chatop company")
-                        .version("v1.0")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-    }
+        @Bean
+        public OpenAPI rentalPortalOpenAPI() {
+                log.info("### SWAGGER CONFIGURATION ###");
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("Rental Portal API")
+                                                .description("Documentation for Rental Portal API to mangage rentals properties and users in Chatop company")
+                                                .version("v1.0"))
+                                .components(new Components()
+                                                .addSecuritySchemes("bearer-key",
+                                                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer").bearerFormat("JWT")));
+        }
 
 }
