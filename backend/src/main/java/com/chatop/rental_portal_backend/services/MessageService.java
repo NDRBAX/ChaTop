38dded7cf62bbe6b4e494a0c6d830102c9f67e01
André Bacaliu <andre.bacaliu@icloud.com>
2025-01-15 22:21:42 +0100

    chore: Add IMessageService import
    - Added import for IMessageService in the MessageService class for
    dependency usage

--- backend/src/main/java/com/chatop/rental_portal_backend/services/MessageService.java
+++ backend/src/main/java/com/chatop/rental_portal_backend/services/MessageService.java
@@ -6,6 +6,7 @@ import com.chatop.rental_portal_backend.dto.MessageRequestDTO;
 import com.chatop.rental_portal_backend.mappers.SendMessageMapper;
 import com.chatop.rental_portal_backend.models.Message;
 import com.chatop.rental_portal_backend.repositories.MessageRepository;
+import com.chatop.rental_portal_backend.services.impl.IMessageService;
 
 import lombok.extern.slf4j.Slf4j;
 
