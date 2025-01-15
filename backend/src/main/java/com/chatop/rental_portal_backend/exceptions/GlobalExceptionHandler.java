37a02116118b1ec54d8482e598413aab96be004f
André Bacaliu <andre.bacaliu@icloud.com>
2025-01-15 19:33:16 +0100

    refactor: Improve exception handling and response structure
    - Simplified exception handling by removing unused annotations
    (@ResponseBody, @ResponseStatus) from exception handlers
    - Updated the exception handlers to return ResponseEntity with
    appropriate HTTP status and content type (JSON)
    - Replaced custom DTO responses with empty JSON bodies for some
    exception cases (e.g., UserAlreadyExistsException,
    UserNotAuthenticatedException)
    - Updated error handling for login and authentication exceptions with
    proper HTTP status (401) and structured responses

--- backend/src/main/java/com/chatop/rental_portal_backend/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/chatop/rental_portal_backend/exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,13 @@
 package com.chatop.rental_portal_backend.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.chatop.rental_portal_backend.dto.EmptyResponseDTO;
 import com.chatop.rental_portal_backend.dto.ResponseMessageDTO;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,30 +17,30 @@ import lombok.extern.slf4j.Slf4j;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    // REGISTER
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST) // 400
-    @ExceptionHandler({ UserAlreadyExistsException.class, InvalidRequestException.class })
-    public EmptyResponseDTO handleUserAlreadyExistsException() {
-        return new EmptyResponseDTO();
+    // REGISTER -> 400
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<Object> handleUserAlreadyExistsException(Exception ex) {
+        log.error("User already exists >>> ERROR : {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body("{}");
     }
 
-    // LOGIN
-    @ResponseBody
-    @ResponseStatus(HttpStatus.UNAUTHORIZED) // 401
-    @ExceptionHandler(InvalidCredentialsException.class)
-    public ResponseMessageDTO handleInvalidCredentialsException(
-            InvalidCredentialsException invalidCredentialsException) {
-        return new ResponseMessageDTO(invalidCredentialsException.getMessage());
+    // LOGIN -> 401
+    @ExceptionHandler(InvalidLoginRequestException.class)
+    public ResponseEntity<ResponseMessageDTO> handleInvalidLoginRequestException(
+            InvalidLoginRequestException invalidCredentialsException) {
+        log.error("Unable to login >>> ERROR : {}", invalidCredentialsException.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ResponseMessageDTO(invalidCredentialsException.getMessage()));
     }
 
-    // GET AUTHENTICATED USER
-    @ResponseBody
-    @ResponseStatus(HttpStatus.UNAUTHORIZED) // 401
-    @ExceptionHandler({ UserNotAuthenticatedException.class, InvalidTokenException.class })
-    public EmptyResponseDTO handleBadCredentialsException(InvalidTokenException ex) {
-        log.error("Handling invalid token exception: {}", ex.getMessage());
-        return new EmptyResponseDTO();
+    // GET AUTHENTICATED USER -> 401
+    @ExceptionHandler(UserNotAuthenticatedException.class)
+    public ResponseEntity<Object> handleUserNotAuthenticatedException(UserNotAuthenticatedException ex) {
+        log.error("User is not authenticated >>> ERROR : {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).contentType(MediaType.APPLICATION_JSON)
+                .body("{}");
     }
 
     // VALIDATION
@@ -61,7 +60,8 @@ public class GlobalExceptionHandler {
 
         String requestURI = request.getRequestURI();
         if (requestURI.contains("/register") || requestURI.contains("/me")) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new EmptyResponseDTO());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON)
+                    .body("{}");
         } else if (requestURI.contains("/login")) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ResponseMessageDTO(errorMessage.toString()));
         }
