9ab3a0492a49beafe1bfa4ac4a17af6de293fa1c
André Bacaliu <andre.bacaliu@icloud.com>
2025-01-15 19:27:36 +0100

    delete: Remove InvalidTokenException
    - Deleted the InvalidTokenException class from the project, which was no
    longer in use

--- backend/src/main/java/com/chatop/rental_portal_backend/exceptions/InvalidTokenException.java
+++
@@ -1,8 +0,0 @@
-package com.chatop.rental_portal_backend.exceptions;
-
-public class InvalidTokenException extends RuntimeException {
-    public InvalidTokenException(String message) {
-        super(message);
-    }
-
-}
