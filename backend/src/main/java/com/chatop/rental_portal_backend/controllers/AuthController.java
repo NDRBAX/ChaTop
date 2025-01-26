19b591a55580c553c45749d5e90fd7a6cb7878c1
André Bacaliu <andre.bacaliu@icloud.com>
2025-01-26 18:32:40 +0100

    refactor: Update AuthController endpoints and API documentation
    - Updated import for IAuthService to use the correct package
    - Refactored API documentation for the "/register" and "/login"
    endpoints, ensuring consistency in response format and descriptions
    - Simplified and unified annotations for request parameters in the
    "/register", "/login", and "/me" endpoints
    - Improved example values for API responses to enhance clarity
    - Changed the return type of the "register" method to
    `ResponseEntity<AuthSuccessDTO>`

--- backend/src/main/java/com/chatop/rental_portal_backend/controllers/AuthController.java
+++ backend/src/main/java/com/chatop/rental_portal_backend/controllers/AuthController.java
@@ -13,7 +13,7 @@ import com.chatop.rental_portal_backend.dto.EmptyResponseDTO;
 import com.chatop.rental_portal_backend.dto.LoginRequestDTO;
 import com.chatop.rental_portal_backend.dto.RegisterRequestDTO;
 import com.chatop.rental_portal_backend.dto.ResponseMessageDTO;
-import com.chatop.rental_portal_backend.services.impl.IAuthService;
+import com.chatop.rental_portal_backend.services.IAuthService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -38,76 +38,37 @@ public class AuthController {
         this.authService = authService;
     }
 
-    @Operation(
-        summary = "Register a new user",
-        description = "Registers a new user in the system.",
-        responses = {
-            @ApiResponse(
-                responseCode = "200",
-                description = "User registered successfully",
-                content = @Content(mediaType = "application/json",
-                schema = @Schema(implementation = AuthSuccessDTO.class),
-                examples = @ExampleObject(value = """
+    @Operation(summary = "Register a new user", description = "Registers a new user in the system.", responses = {
+            @ApiResponse(responseCode = "200", description = "User registered successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthSuccessDTO.class), examples = @ExampleObject(value = """
                     {
                     "token" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MzY5NjQyMDAsInN1YiI6InRlc3RAbWFpbC5jb20iLCJpYXQiOjE3MzY5NjA2MDB9.nTeRgSgFXIBqucayyjNbVYEBwv4ZYBn6cTDZS7tVz-k"
                     }
-                    """,
-                    summary = "User registered successfully"))),
-            @ApiResponse(
-                responseCode = "400",
-                description = "User cannot be registred with the provided data",
-                content = @Content(mediaType = "application/json",
-                schema = @Schema(implementation = EmptyResponseDTO.class),
-                examples = @ExampleObject(value = """
+                    """, summary = "User registered successfully"))),
+            @ApiResponse(responseCode = "400", description = "User cannot be registred with the provided data", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmptyResponseDTO.class), examples = @ExampleObject(value = """
                     {}
-                    """,
-                    summary = "User cannot be registered with the provided data")))
+                    """, summary = "User cannot be registered with the provided data")))
     })
-    @Parameter(
-        name = "registerRequest",
-        description = "The user registration request",
-        content = @Content(mediaType = "application/json",
-        schema = @Schema(implementation = RegisterRequestDTO.class)))
+    @Parameter(name = "registerRequest", description = "The user registration request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = RegisterRequestDTO.class)))
     @PostMapping("/register")
-    public ResponseEntity<Object> register(@Valid @RequestBody RegisterRequestDTO registerRequest) {
+    public ResponseEntity<AuthSuccessDTO> register(@Valid @RequestBody RegisterRequestDTO registerRequest) {
         String token = authService.registerUser(registerRequest);
         log.info(">>> USER REGISTERED WITH EMAIL >>> {} <<<", registerRequest.getEmail());
         return ResponseEntity.ok(new AuthSuccessDTO(token));
     }
 
-    @Operation(
-        summary = "Login a user",
-        description = "Logs in a user in the system.",
-        responses = {
-            @ApiResponse(
-                responseCode = "200",
-                description = "User logged in successfully",
-                content = @Content(mediaType = "application/json",
-                schema = @Schema(implementation = AuthSuccessDTO.class),
-                examples = @ExampleObject(value = """
+    @Operation(summary = "Login a user", description = "Logs in a user in the system.", responses = {
+            @ApiResponse(responseCode = "200", description = "User logged in successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthSuccessDTO.class), examples = @ExampleObject(value = """
                     {
                     "token" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MzY5NjQyMDAsInN1YiI6InRlc3RAbWFpbC5jb20iLCJpYXQiOjE3MzY5NjA2MDB9.nTeRgSgFXIBqucayyjNbVYEBwv4ZYBn6cTDZS7tVz-k"
                     }
-                    """,
-                    summary = "User logged in successfully"))),
-
-            @ApiResponse(
-                responseCode = "401",
-                description = "Unable to login user with provided credentials",
-                content = @Content(mediaType = "application/json",
-                schema = @Schema(implementation = ResponseMessageDTO.class),
-                examples = @ExampleObject(value = """
+                    """, summary = "User logged in successfully"))),
+            @ApiResponse(responseCode = "401", description = "Unable to login user with provided credentials", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessageDTO.class), examples = @ExampleObject(value = """
                     {
                     "message" : "Unable to login user with provided credentials"
                     }
-                    """,
-                    summary = "Unable to login user with provided credentials")))
+                    """, summary = "Unable to login user with provided credentials")))
     })
-    @Parameter(
-        name = "loginRequest",
-        description = "The user login request",
-        content = @Content(mediaType = "application/json",
-        schema = @Schema(implementation = LoginRequestDTO.class)))
+    @Parameter(name = "loginRequest", description = "The user login request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = LoginRequestDTO.class)))
     @PostMapping("/login")
     public ResponseEntity<AuthSuccessDTO> login(@Valid @RequestBody LoginRequestDTO loginRequest) {
         String token = authService.loginUser(loginRequest);
@@ -115,39 +76,21 @@ public class AuthController {
         return ResponseEntity.ok(new AuthSuccessDTO(token));
     }
 
-    @Operation(
-        summary = "Get authenticated user",
-        description = "Fetches the authenticated user details.",
-        responses = {
-            @ApiResponse(responseCode = "200",
-            description = "User details retrieved successfully",
-            content = @Content(mediaType = "application/json",
-            schema = @Schema(implementation = AuthenticatedUserDTO.class),
-            examples = @ExampleObject(value = """
+    @Operation(summary = "Get authenticated user", description = "Fetches the authenticated user details.", responses = {
+            @ApiResponse(responseCode = "200", description = "User details retrieved successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthenticatedUserDTO.class), examples = @ExampleObject(value = """
                     {
-                        "id": 1,
-                        "name": "John Doe",
-                        "email": "john_doe@mail.com",
-                        "created_at": "2025-01-01 12:00:00",
-                        "updated_at": "2025-01-01 12:00:00"
+                            "id": 1,
+                            "name": "John Doe",
+                            "email": "john_doe@mail.com",
+                            "created_at": "2025-01-01 12:00:00",
+                            "updated_at": "2025-01-01 12:00:00"
                     }
-                    """,
-                    summary = "User details retrieved successfully"))),
-            @ApiResponse(
-                responseCode = "401",
-                description = "Unauthorized",
-                content = @Content(mediaType = "application/json",
-                schema = @Schema(implementation = EmptyResponseDTO.class),
-                examples = @ExampleObject(value = """
+                    """, summary = "User details retrieved successfully"))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmptyResponseDTO.class), examples = @ExampleObject(value = """
                     {}
-                    """,
-                    summary = "Unauthorized")))
+                    """, summary = "Unauthorized")))
     })
-    @Parameter(
-        name = "authenticatedUser",
-        description = "The authenticated user details",
-        content = @Content(mediaType = "application/json",
-        schema = @Schema(implementation = AuthenticatedUserDTO.class)))
+    @Parameter(name = "authenticatedUser", description = "The authenticated user details", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthenticatedUserDTO.class)))
     @SecurityRequirement(name = "bearer-key")
     @GetMapping("/me")
     public ResponseEntity<AuthenticatedUserDTO> getAuthenticatedUser() {
