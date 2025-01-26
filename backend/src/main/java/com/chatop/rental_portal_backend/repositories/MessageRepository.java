8a94070e5f782b02e492ad818f8975b22eeb411b
André Bacaliu <andre.bacaliu@icloud.com>
2025-01-26 18:37:11 +0100

    fix: Add @Repository annotation to repository interfaces
    - Added the @Repository annotation to the MessageRepository,
    RentalRepository, and UserRepository interfaces to properly define them
    as Spring Data JPA repositories.

--- backend/src/main/java/com/chatop/rental_portal_backend/repositories/MessageRepository.java
+++ backend/src/main/java/com/chatop/rental_portal_backend/repositories/MessageRepository.java
@@ -1,8 +1,10 @@
 package com.chatop.rental_portal_backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.chatop.rental_portal_backend.models.Message;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 }
--- backend/src/main/java/com/chatop/rental_portal_backend/repositories/UserRepository.java
+++ backend/src/main/java/com/chatop/rental_portal_backend/repositories/UserRepository.java
@@ -1,10 +1,13 @@
 package com.chatop.rental_portal_backend.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.chatop.rental_portal_backend.models.User;
-
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.chatop.rental_portal_backend.models.User;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
