07fb1b7d8b8e91d5b7c51ad85cc046f2d864b9ab
André Bacaliu <andre.bacaliu@icloud.com>
2025-01-26 18:31:41 +0100

    refactor: Enhance Swagger configuration with dynamic server URL for development
    - Replaced hardcoded server URL with dynamic value using @Value
    annotation for the dev environment
    - Added a 'devUrl' property to be injected from application properties
    - Updated OpenAPI bean method to include server URL and improve Swagger
    documentation for the development environment
    - Added detailed comments to the Swagger configuration method for
    clarity

--- backend/src/main/java/com/chatop/rental_portal_backend/configuration/SwaggerConfig.java
+++ backend/src/main/java/com/chatop/rental_portal_backend/configuration/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.chatop.rental_portal_backend.configuration;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,24 +11,44 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.extern.slf4j.Slf4j;
+
 
 @Slf4j
 @Profile("dev")
 @Configuration
 public class SwaggerConfig {
-        @Bean
-        public OpenAPI rentalPortalOpenAPI() {
-                log.info("### SWAGGER CONFIGURATION ###");
-                return new OpenAPI()
-                                .info(new Info()
-                                                .title("Rental Portal API")
-                                                .description("Documentation for Rental Portal API to mangage rentals properties and users in Chatop company")
-                                                .version("v1.0"))
-                                .components(new Components()
-                                                .addSecuritySchemes("bearer-key",
-                                                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
-                                                                                .scheme("bearer").bearerFormat("JWT")));
-        }
 
+    @Value("${rental_portal.openapi.dev-url}")
+    private String devUrl;
+
+    /**
+     * Configures the OpenAPI object.
+     * 
+     * This method configures the OpenAPI object to provide information about the API
+     * and to define the security scheme used by the API.
+     * 
+     * @return the configured OpenAPI object.
+     */
+    @Bean
+    public OpenAPI rentalPortalOpenAPI() {
+        log.info("### SWAGGER CONFIGURATION ###");
+        Server devServer = new Server().url(devUrl).description("Server URL in Development environment");
+    
+        Info info = new Info()
+            .title("Rental Portal API")
+            .version("1.0")
+            .description("Documentation for Rental Portal API to manage rentals properties and users in Chatop company");
+        
+        Components components = new Components()
+            .addSecuritySchemes("bearer-key",
+                new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")
+            );
+
+        return new OpenAPI().info(info).servers(List.of(devServer)).components(components);
+    }
 }
